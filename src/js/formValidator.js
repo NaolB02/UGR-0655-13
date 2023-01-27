49ebc6ace78b91898605f081b687cdf89bfa582e
NaolB02 <106165221+NaolB02@users.noreply.github.com>
2023-01-27 23:22:40 +0300

    Form Validator

--- src/js/formValidator.js
+++ src/js/formValidator.js
@@ -1,15 +1,11 @@
-(function() {
-    var forms = document.querySelector(".form");
+var forms = document.querySelectorAll(".needs-validation");
+Array.prototype.slice.call(forms).forEach(function(form) {
+    form.addEventListener("submit", function(event) {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+    }, false);
 
-    Array.prototype.slice.call(forms)
-        .forEach(function(form) {
-            form.addEventListener('submit', function(event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
-
-                form.classList.add('was-validated')
-            }, false)
-        })
-})
+});
